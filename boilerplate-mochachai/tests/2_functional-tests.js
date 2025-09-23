b838c0617655e0c681c9588fcb9640586f785396
adtybrhn <burhan.aditya@bni.co.id>
2025-09-23 14:59:46 +0700

    fix unit test

--- boilerplate-mochachai/tests/2_functional-tests.js
+++ boilerplate-mochachai/tests/2_functional-tests.js
@@ -1,85 +1,96 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const server = require('../server');
+const server = require("../server");
 
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
-suite('Functional Tests', function () {
-  this.timeout(5000);
-  suite('Integration tests with chai-http', function () {
+suite("Functional Tests", function () {
+  suite("Integration tests with chai-http", function () {
     // #1
-    test('Test GET /hello with no name', function (done) {
+    test("Test GET /hello with no name", function (done) {
       chai
         .request(server)
-        .keepOpen()
-        .get('/hello')
+        .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
     // #2
-    test('Test GET /hello with your name', function (done) {
+    test("Test GET /hello with your name", function (done) {
       chai
         .request(server)
-        .keepOpen()
-        .get('/hello?name=xy_z')
+        .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello xy_z");
           done();
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
-        .keepOpen()
-        .put('/travellers')
-
+        .put("/travellers")
+        .send({ surname: "Colombo" })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Cristoforo");
+          assert.equal(res.body.surname, "Colombo");
           done();
         });
     });
     // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+    test('send {surname: "da Verrazzano"}', function (done) {
+      chai
+        .request(server)
+        .put("/travellers")
+        .send({ surname: "da Verrazzano" })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
     });
   });
 });
 
-const Browser = require('zombie');
+const Browser = require("zombie");
+Browser.site = "localhost:3000";
 
-suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
-
-
-
-  suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
-    });
+suite("Functional Tests with Zombie.js", function () {
+  const browser = new Browser();
+  suiteSetup(function (done) {
+    return browser.visit("/", done);
   });
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+    test('submit "surname" : "Colombo" - write your e2e test...', async function () {
+      await browser.fill("surname", "Colombo");
+      await browser.pressButton("submit");
 
-      done();
+      browser.assert.success();
+      browser.assert.text("span#name", "Cristoforo");
+      browser.assert.text("span#surname", "Colombo");
+      browser.assert.elements("span#dates", 1);
     });
-    // #6
-    test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
 
-      done();
+    // #6
+    test('submit "surname" : "Vespucci" - write your e2e test...', async function () {
+      await browser.fill("surname", "Vespucci");
+      await browser.pressButton("submit");
+
+      browser.assert.success();
+      browser.assert.text("span#name", "Amerigo");
+      browser.assert.text("span#surname", "Vespucci");
+      browser.assert.elements("span#dates", 1);
     });
   });
 });
