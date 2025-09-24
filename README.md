ab996a0a2d7cb9a7963ce9b93c705981e0cc35a0
adtybrhn <burhan.aditya@bni.co.id>
2025-09-24 16:22:27 +0700

    issue-tracker

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Quality Assurance with Chai
+# Issue Tracker
 
-This is the boilerplate for the Quality Assurance with Chai lessons. Instructions for completing these lessons start at https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-and-testing-with-chai/
+This is the boilerplate for the Issue Tracker project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/issue-tracker
--- server.js
+++ server.js
@@ -1,119 +1,65 @@
-"use strict";
-const express = require("express");
-const app = express();
+'use strict';
 
-const cors = require("cors");
-const runner = require("./test-runner");
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+require('dotenv').config();
 
-const bodyParser = require("body-parser");
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
+
+let app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+//Sample front-end
+app.route('/:project/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/issue.html');
+  });
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
 });
 
-app.use(express.static(__dirname + "/public"));
-
-app.get("/hello", function (req, res) {
-  const name = req.query.name || "Guest";
-  res.type("txt").send("hello " + name);
+//Start our server and tests!
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        console.log('Tests are not valid:');
+        console.error(e);
+      }
+    }, 3500);
+  }
 });
 
-const travellers = function (req, res) {
-  let data = {};
-  if (req.body && req.body.surname) {
-    switch (req.body.surname.toLowerCase()) {
-      case "polo":
-        data = {
-          name: "Marco",
-          surname: "Polo",
-          dates: "1254 - 1324",
-        };
-        break;
-      case "colombo":
-        data = {
-          name: "Cristoforo",
-          surname: "Colombo",
-          dates: "1451 - 1506",
-        };
-        break;
-      case "vespucci":
-        data = {
-          name: "Amerigo",
-          surname: "Vespucci",
-          dates: "1454 - 1512",
-        };
-        break;
-      case "da verrazzano":
-      case "verrazzano":
-        data = {
-          name: "Giovanni",
-          surname: "da Verrazzano",
-          dates: "1485 - 1528",
-        };
-        break;
-      default:
-        data = {
-          name: "unknown",
-        };
-    }
-  }
-  res.json(data);
-};
-
-app.route("/travellers").put(travellers);
-
-let error;
-app.get(
-  "/_api/get-tests",
-  cors(),
-  function (req, res, next) {
-    if (error) return res.json({ status: "unavailable" });
-    next();
-  },
-  function (req, res, next) {
-    if (!runner.report) return next();
-    res.json(testFilter(runner.report, req.query.type, req.query.n));
-  },
-  function (req, res) {
-    runner.on("done", function (report) {
-      process.nextTick(() =>
-        res.json(testFilter(runner.report, req.query.type, req.query.n)),
-      );
-    });
-  },
-);
-
-app.listen(3000, function () {
-  console.log("Listening on port " + 3000);
-  console.log("Running Tests...");
-  setTimeout(function () {
-    try {
-      runner.run();
-    } catch (e) {
-      error = e;
-      console.log("Tests are not valid:");
-      console.log(error);
-    }
-  }, 1500);
-});
-
-module.exports = app; // for testing
-
-function testFilter(tests, type, n) {
-  let out;
-  switch (type) {
-    case "unit":
-      out = tests.filter((t) => t.context.match("Unit Tests"));
-      break;
-    case "functional":
-      out = tests.filter((t) => t.context.match("Functional Tests"));
-      break;
-    default:
-      out = tests;
-  }
-  if (n !== undefined) {
-    return out[n] || out;
-  }
-  return out;
-}
+module.exports = app; //for testing
